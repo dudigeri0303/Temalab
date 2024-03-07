479e078471d85fc56cef103b0dcfd55f0b783e74
BiroBalint <balintbiro76@gmail.com>
2024-03-07 12:31:24 +0100

    React_setup

---
+++ TemalabFrontEnd/src/common/SectionWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SectionWrapper() {
+  return (
+    <div className="backgroundc">
+      <div className="mx-auto max-w-[1200px]">{children}</div>
+    </div>
+  );
+}
