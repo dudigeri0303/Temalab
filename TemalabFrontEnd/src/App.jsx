8816f8ce668c6398c2df0816faafe10d8da16827
RolandRichardSimon <mr.probike@gmail.com>
2024-03-09 13:53:34 +0100

    Footer nagyjából kész + hozzáadtam egy körbvágott logo-t a projecthez

--- TemalabFrontEnd/src/App.jsx
+++ TemalabFrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './sections/Login';
+import Footer from './components/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
               <Route path="/" element={<Login/>} />
             </Routes>
       </Router>
+      <Footer /> {/* Elvileg így minden oldalra render-elve lesz*/}
     </>
   )
 }
