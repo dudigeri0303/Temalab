1199c466161d5f1923c83d4a819a2bd039345657
BiroBalint <balintbiro76@gmail.com>
2024-03-10 22:27:05 +0100

    Navbar_And_Login_Desgin_Kinda_Done
    Talán bugos, de idk xd

--- TemalabFrontEnd/src/App.jsx
+++ TemalabFrontEnd/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './sections/Login';
 import CreateRestaurant from './sections/CreateRestaurant';
 import Profile from './sections/Profile';
-
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Router>
@@ -17,7 +14,7 @@ function App() {
               <Route path="/" element={<Login/>} />
               <Route path="/createRestaurant" element={<CreateRestaurant/>} /> {/*Elérhető: http://localhost:valami_szám/createRestaurant*/}
               <Route path="/profile" element={<Profile/>} /> {/*Elérhető: http://localhost:valami_szám/Profile*/}
-        
+              <Route path='/navbartest' element={<Navbar/>} />
             </Routes>
       </Router>
       <Footer /> {/* Elvileg így minden oldalra render-elve lesz*/}
