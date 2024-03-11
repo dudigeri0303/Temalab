0f8996afe54e467f798705606cc17464650808f5
Sárközi Tamás <st.tomi1995@gmail.com>
2024-03-11 22:22:30 +0100

    OwnerNavbar

--- TemalabFrontEnd/src/App.jsx
+++ TemalabFrontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './sections/Login';
 import RegisterForm from './sections/Register';
+import OwnerNavbar from './components/OwnerNavbar';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <Router>
             <Routes>
+              <Route path="/" element={<OwnerNavbar/>} />
               <Route path="/" element={<RegisterForm/>} />
               <Route path="/" element={<Login/>} />
             </Routes>
