81311697238b403670e48678cca03b72a9bee2bc
RolandRichardSimon <mr.probike@gmail.com>
2024-03-13 09:59:04 +0100

    Customer main + Owner main basics added

--- TemalabFrontEnd/src/sections/Template.jsx
+++ TemalabFrontEnd/src/sections/Template.jsx
@@ -41,12 +41,13 @@ export default function Template() {
           </div>
         </div>
       </section>
+
       <section id="main" className="container py-2">
         <div className="row div-card">
           <div className="col-md-4">
             <div className="mb-3">
               <label htmlFor="restaurantName" className="card-text">
-                FirstName LastName
+                Username
               </label>
             </div>
           </div>
@@ -85,19 +86,23 @@ export default function Template() {
               </div>
               <div className="col-md-4">
                 <label htmlFor="stars" className="card-text">
-                ★★★★★
+                  ★★★★★
                 </label>
               </div>
             </div>
             <div className="row">
-              <div className="col" style={{ alignSelf: 'flex-end'}}>
+              <div className="col d-flex align-items-end">
                 <label htmlFor="restaurantStyle" className="card-Altext">
                   Étterem stílusa
                 </label>
               </div>
               <div className="col"></div>
               <div className="col">
-                <button type="button" className="cardbtn float-end" style={{ margin: "0" }}>
+                <button
+                  type="button"
+                  className="cardbtn float-end"
+                  style={{ margin: "0" }}
+                >
                   Eltávolítás
                 </button>
               </div>
@@ -112,6 +117,76 @@ export default function Template() {
           </div>
         </div>
       </section>
+
+      <section id="main" className="container py-2">
+        <div className="row div-card">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm d-flex align-items-center">
+                <label htmlFor="restaurantStyle" className="card-text">
+                  Étterem neve
+                </label>{" "}
+              </div>
+              <div className="col-sm d-flex align-items-center">
+                <label htmlFor="restaurantStyle" className="card-text">
+                  ★★★★★
+                </label>{" "}
+              </div>
+              <div className="col-sm d-flex align-items-center">
+                <label htmlFor="restaurantStyle" className="card-Altext">
+                  Étterem címe
+                </label>{" "}
+              </div>
+              <div className="col-sm">
+                <button
+                  type="button"
+                  className="cardbtn float-end"
+                >
+                  Szerkesztés
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="main" className="container py-2">
+        <div className="row div-card">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <label htmlFor="restaurantName" className="card-text">
+                  Étterem név
+                </label>
+              </div>
+              <div className="col-md-6 d-flex justify-content-end">
+                <label htmlFor="stars" className="card-text">
+                  ★★★★★
+                </label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col d-flex align-items-end">
+                <label htmlFor="restaurantStyle" className="card-Altext">
+                  Étterem stílusa
+                </label>
+              </div>
+              <div className="col d-flex justify-content-end">
+              <button className="likebtn"><img src="./public/likebtn.png" style={{ height: '40px', width: '40px' }}/></button>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <label htmlFor="restaurantPlace" className="card-Altext">
+                  Étterem helye
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
     </>
   );
 }
