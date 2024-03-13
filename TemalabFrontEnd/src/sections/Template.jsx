6dfbc1f348c2f8108bb819dab10cb1a2afdeefac
RolandRichardSimon <mr.probike@gmail.com>
2024-03-13 10:09:14 +0100

    owner tables done

--- TemalabFrontEnd/src/sections/Template.jsx
+++ TemalabFrontEnd/src/sections/Template.jsx
@@ -138,10 +138,7 @@ export default function Template() {
                 </label>{" "}
               </div>
               <div className="col-sm">
-                <button
-                  type="button"
-                  className="cardbtn float-end"
-                >
+                <button type="button" className="cardbtn float-end">
                   Szerkesztés
                 </button>
               </div>
@@ -172,7 +169,12 @@ export default function Template() {
                 </label>
               </div>
               <div className="col d-flex justify-content-end">
-              <button className="likebtn"><img src="./public/likebtn.png" style={{ height: '40px', width: '40px' }}/></button>
+                <button className="likebtn">
+                  <img
+                    src="./public/likebtn.png"
+                    style={{ height: "40px", width: "40px" }}
+                  />
+                </button>
               </div>
             </div>
             <div className="row">
@@ -186,7 +188,34 @@ export default function Template() {
         </div>
       </section>
 
-
+      <section id="main" className="container py-2">
+        <div className="row div-card">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm d-flex align-items-center">
+                <label htmlFor="restaurantPlace" className="card-Altext">
+                  AsztalID
+                </label>
+              </div>
+              <div className="col-sm d-flex align-items-center">
+                <label htmlFor="restaurantPlace" className="card-Altext">
+                  Férőhely
+                </label>
+              </div>
+              <div className="col-sm d-flex align-items-center">
+                <label htmlFor="restaurantPlace" className="card-Altext">
+                  IsFoglalt
+                </label>
+              </div>
+              <div className="col-sm">
+                <button type="button" className="cardbtn float-end">
+                  Töröl
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
