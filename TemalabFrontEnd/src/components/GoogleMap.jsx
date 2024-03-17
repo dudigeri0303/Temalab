ddfcf770ff0227a52c4bddcfbbadf6b8ddfca148
BiroBalint <balintbiro76@gmail.com>
2024-03-17 20:50:50 +0100

    fos

--- TemalabFrontEnd/src/components/GoogleMap.jsx
+++ TemalabFrontEnd/src/components/GoogleMap.jsx
@@ -1,46 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { geocodeByAddress, getLatLng } from '@react-google-maps/api';
+import React from 'react';
 
-const MapContainer = () => {
-  const [center, setCenter] = useState({ lat: 0, lng: 0 });
-  const [address, setAddress] = useState("Budapest");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const results = await geocodeByAddress(address);
-        const latLng = await getLatLng(results[0]);
-        setCenter(latLng);
-        console.log(address)
-      } catch (error) {
-        console.error('Error fetching and parsing data', error);
-      }
-    };
-
-    fetchData();
-  }, [address]);
-
-  const mapStyles = {
-    height: "400px",
-    width: "100%"
-  };
-
-  return (
-    <div>
-      <LoadScript
-        googleMapsApiKey="AIzaSyAkCFbU5vtVRjOpRh77Z4TBEn-zY8i2X_4"
-      >
-        <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={13}
-          center={center}
-        >
-          <Marker position={center} />
-        </GoogleMap>
-      </LoadScript>
-    </div>
+export default function GoogleMap(){
+  return(
+    <>
+      <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJMeAUNardQUcRFdRH4YNCqyI&key=AIzaSyAkCFbU5vtVRjOpRh77Z4TBEn-zY8i2X_4"></iframe>
+    </>
   )
 }
-
-export default MapContainer;
