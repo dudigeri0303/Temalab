e90d930184645a4a23d54f83e268176ab99e49bd
RolandRichardSimon <mr.probike@gmail.com>
2024-03-29 15:32:43 +0100

    Example added to the pop up review

--- TemalabFrontEnd/src/sections/Template.jsx
+++ TemalabFrontEnd/src/sections/Template.jsx
@@ -41,15 +41,15 @@ export default function Template() {
           <div className="container">
             <div className="row">
               <div className="col-sm-6 text-center">
-                <label className="card-text">Felhasználónév</label>
+                <label className="card-Altext">Felhasználónév</label>
               </div>
               <div className="col-sm-6 text-center">
-                <label className="card-text">★★★★★</label>
+                <label className="card-Altext">★★★★★</label>
               </div>
             </div>
             <div className="row">
               <div className="col-sm-12 text-center">
-                <label className="card-text">Vélemény</label>
+                <label className="card-Altext">Vélemény</label>
               </div>
             </div>
           </div>
