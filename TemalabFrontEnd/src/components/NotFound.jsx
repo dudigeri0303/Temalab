459a481b04c4e2ad260429ecfb174c84f7acbb79
Sárközi Tamás <st.tomi1995@gmail.com>
2024-05-07 12:56:09 +0200

    userlist+404gomb

--- TemalabFrontEnd/src/components/NotFound.jsx
+++ TemalabFrontEnd/src/components/NotFound.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+const Login = () => {
+  window.open("/", "_self");
+};
+
 function NotFound() {
   return (
     <div className="min-vh-100">
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
+      <p>Are you possibly not logged in?</p>
+      <button className='avgbtn' type="button" onClick={Login}>Log in</button>
     </div>
   );
 }
+
 
 export default NotFound;
