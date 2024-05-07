1e99ed14b988a1abd5b410b4cd4b59cd749f465d
RolandRichardSimon <mr.probike@gmail.com>
2024-05-07 19:49:39 +0200

    input added for now

--- TemalabFrontEnd/src/components/CardAddReview.jsx
+++ TemalabFrontEnd/src/components/CardAddReview.jsx
@@ -1,6 +1,9 @@
 import "../App.css";
 
 export default function CardAddReview() {
+
+
+  
   return (
     <>
       <section id="main" className="container py-2">
@@ -9,10 +12,11 @@ export default function CardAddReview() {
 
             <div className="row">
               <div className="text-center">
-              <label className="card-Altext">★★★★★</label>
+              <label className="card-Altext">Értékelés</label>
+              <input id="rate" type="number" min={1} max={5}/>
               </div>
               <div className="col-sm-12 text-center">
-                <label className="card-Altext">Vélemény helye</label>
+                <textarea id="review" placeholder="Vélemény" maxLength={250}></textarea>
               </div>
             </div>
           </div>
