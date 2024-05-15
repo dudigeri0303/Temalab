c85e26aa08ac45a7e16c9420324c7c74237182bd
RolandRichardSimon <mr.probike@gmail.com>
2024-05-15 12:27:07 +0200

    asztal hozzadasa modal added

--- TemalabFrontEnd/src/sections/OwnerManageTables.jsx
+++ TemalabFrontEnd/src/sections/OwnerManageTables.jsx
@@ -2,18 +2,19 @@ import "../App.css";
 import React, { useEffect, useState } from "react";
 import OwnerNavbar from "../components/OwnerNavbar";
 import CardOwnerTables from "../components/CardOwnerTables";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
 import CheckAuth from "../common/CheckAuth";
+import Table from "../components/Table";
 
 export default function OwnerManageTables() {
-
-    const [tables, setTables] = useState([]);
-    const { id } = useParams();
-    const navigate = useNavigate(); 
+  const [tables, setTables] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.title = " Asztalok Kezelése | DineTab";
-    CheckAuth("owner",navigate)
+    CheckAuth("owner", navigate);
     getTables();
   }, []);
 
@@ -29,7 +30,8 @@ export default function OwnerManageTables() {
 
     try {
       const response = await fetch(
-        `https://localhost:7114/api/Table/listTablesByRestaurantId?restaurantId=${id}`, requestOptions
+        `https://localhost:7114/api/Table/listTablesByRestaurantId?restaurantId=${id}`,
+        requestOptions
       );
       const data = await response.json();
       setTables(data);
@@ -40,13 +42,30 @@ export default function OwnerManageTables() {
 
   return (
     <>
-    <OwnerNavbar/>
+      <OwnerNavbar />
 
-    <div>
-          {tables.map((table) => (
-            <CardOwnerTables key={table.id} id={table.id} numOfSeats={table.numOfSeats} isReserved={table.isReserved.toString()} />
-          ))}
-          </div>
+      <div>
+        {tables.map((table) => (
+          <CardOwnerTables
+            key={table.id}
+            id={table.id}
+            numOfSeats={table.numOfSeats}
+            isReserved={table.isReserved.toString()}
+          />
+        ))}
+      </div>
+
+      <Table showModal={showModal} setShowModal={setShowModal}></Table>
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center">
+          <button
+            className="btnstyle avgbtn my-3 py-3"
+            onClick={() => setShowModal(true)}
+          >
+            Asztal hozzáadása
+          </button>
+        </div>
+      </div>
     </>
   );
 }
